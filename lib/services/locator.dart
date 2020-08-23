8f548a171e32e9be4d7d74c0fb387d1c6c25aeca
Imgkl <gokul@1stmain.co>
2020-08-23 10:37:34 +0530

    removed unused file

--- lib/services/locator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:rude/services/navigation_service.dart';
-import 'package:get_it/get_it.dart';
-
-final locator = GetIt.I;
-void setupLocator() {
-  locator.registerLazySingleton(() => NavigationService());
-}
